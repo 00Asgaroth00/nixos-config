3b2dae6c2820d213b26eb3237e9f0d1858abc4ff
Asgaroth <00asgaroth00@gmail.com>
2024-01-16 11:13:33 +0000

    Revert "variable interpolation between nix and shell script"
    This reverts commit 008f5dad8c0c2295cfe1b155e5f1a4f3de97c920.

--- nixos/thanos/zfs.nix
+++ nixos/thanos/zfs.nix
@@ -24,11 +24,11 @@
 
   boot.loader.grub.extraInstallCommands = ''
     CMD_MKTEMP=$(command -v mktemp)
-    if [[ -n "$CMD_MKTEMP" ]]; then
+    if [[ -n "${CMD_MKTEMP}" ]]; then
       echo "mktemp not found, please install it";
       exit 1
     else
-      ESP_MIRROR=$($CMD_MKTEMP -d)
+      ESP_MIRROR=$(${CMD_MKTEMP} -d)
       cp -r /boot/efi/EFI $ESP_MIRROR
       for i in /boot/efis/*; do
         cp -r $ESP_MIRROR/EFI $i
