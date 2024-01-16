91812f40bebb578048fcf4cf23c7afa76d8ad9e9
Asgaroth <00asgaroth00@gmail.com>
2024-01-16 09:43:48 +0000

    Initial commit

---
+++ nixos/thanos/configuration.nix
@@ -0,0 +1,134 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  username,
+  ...
+}: {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/nixos):
+    # outputs.nixosModules.example
+
+    # Or modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+
+    inputs.hardware.nixosModules.dell-xps-13-7390
+    ./hardware-configuration.nix
+    ./zfs.nix
+
+    ../global/locale.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  # This will add each flake input as a registry
+  # To make nix3 commands consistent with your flake
+  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+  # This will additionally add your inputs to the system's legacy channels
+  # Making legacy nix commands consistent as well, awesome!
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc =
+    lib.mapAttrs'
+    (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
+
+  nix.settings = {
+    # Enable flakes and new 'nix' command
+    experimental-features = "nix-command flakes";
+    # Deduplicate and optimize nix store
+    auto-optimise-store = true;
+  };
+
+  # FIXME: Add the rest of your current configuration
+
+  networking.hostName = "thanos";
+  networking.useDHCP = lib.mkForce true;
+  networking.networkmanager.enable = true;
+
+  # TODO: This is just an example, be sure to use whatever bootloader you prefer
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+
+  hardware.enableRedistributableFirmware = true;
+
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "uk";
+  };
+
+  # systemd.sleep.extraConfig = "SuspendState=freeze";
+
+  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
+  users.users = {
+    root = {
+      hashedPassword = '$y$j9T$MWL7ZiI04tv19poUTs./e1$DnFUJwg.lHgoypCyyl8s/NxVsQseq16KLFQajBpmMIB';
+    };
+    ${username} = {
+      # TODO: You can set an initial password for your user.
+      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
+      # Be sure to change it (using passwd) after rebooting!
+      # run 'mkpasswd -m yescrypt'
+      # initialPassword = "correcthorsebatterystaple";
+      hashedPassword = '$y$j9T$LOW.UUJZdl94Miro2rPYi0$X7HGYuYWgrG87fGU9F31lElzJSrXC3rwYozqYbc0fZ6';
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      ];
+      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      extraGroups = ["wheel"];
+    };
+  };
+
+  # This setups a SSH server. Very important if you're setting up a headless system.
+  # Feel free to remove if you don't need it.
+  services.openssh = {
+    enable = true;
+    settings = {
+      # Forbid root login through SSH.
+      PermitRootLogin = "no";
+      # Use keys only. Remove if you want to SSH using password (not recommended)
+      # PasswordAuthentication = false;
+    };
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.11";
+}
---
+++ nixos/thanos/zfs.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "ef08dd92";
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.zfs.devNodes = "/dev/disk/by-partlabel";
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader.generationsDir.copyKernels = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.copyKernels = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.zfsSupport = true;
+
+  boot.loader.grub.extraPrepareConfig = ''
+    mkdir -p /boot/efis
+    for i in  /boot/efis/*; do mount $i ; done
+
+    mkdir -p /boot/efi
+    mount /boot/efi
+  '';
+
+  boot.loader.grub.extraInstallCommands = ''
+    ESP_MIRROR=$(mktemp -d)
+    cp -r /boot/efi/EFI $ESP_MIRROR
+    for i in /boot/efis/*; do
+      cp -r $ESP_MIRROR/EFI $i
+    done
+    rm -rf $ESP_MIRROR
+  '';
+
+  boot.loader.grub.devices = [
+    "/dev/nvme0n1"
+  ];
+
+  # users.users.root.initialHashedPassword = "$6$JwCXs8pAVWXO.7fH$3aQlsAFgeDnw9N0n.bIG5ZMW1/krpx42HX3PADsrydhB1y0gYO8Cam/RotEQW9I8izxzngdKuvBNQbJfnaWGm1";
+
+}
