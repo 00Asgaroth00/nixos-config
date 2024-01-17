877697c1cd29e91e712372f95ac771b30a97a69d
Asgaroth <00asgaroth00@gmail.com>
2024-01-17 16:49:45 +0000

    enable zfs auto scrub and trim services

--- nixos/thanos/configuration.nix
+++ nixos/thanos/configuration.nix
@@ -125,6 +125,12 @@
     };
   };
 
+  services.zfs = {
+    autoScrub.enable = true;
+    trim.enable = true;
+  };
+
+  
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
