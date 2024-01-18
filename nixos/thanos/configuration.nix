0b757d5fa6376f2f751d4b66c7d388c2e7301d9c
Asgaroth <00asgaroth00@gmail.com>
2024-01-18 17:47:46 +0000

    more testing

--- nixos/thanos/configuration.nix
+++ nixos/thanos/configuration.nix
@@ -192,6 +192,13 @@
 
   services.auto-cpufreq.enable = true;
 
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  services.blueman.enable = true;
+
   xdg.portal = {
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
