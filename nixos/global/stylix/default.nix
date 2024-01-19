ecaa684af55c35581cf1e7f05997f9472732e41c
Asgaroth <00asgaroth00@gmail.com>
2024-01-19 21:32:22 +0000

    testing

--- nixos/global/stylix/default.nix
+++ nixos/global/stylix/default.nix
@@ -2,10 +2,18 @@
 
   stylix = {
     base16Scheme = "${pkgs.base16-schemes}/share/themes/${colour_scheme}.yaml";
+    
     image = pkgs.fetchurl {
       url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
       sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
     };
+    
+    fonts = {
+      serif = config.fontProfiles.monospace.family;
+      sansSerif = config.fontProfiles.monospace.family;
+      monospace = config.fontProfiles.monospace.family;
+    };
+
   };
 
 }
