e08815c92444c1a33ef427872cd8a290ab5b732b
Asgaroth <00asgaroth00@gmail.com>
2024-01-19 19:33:51 +0000

    testing

--- nixos/global/fonts/default.nix
+++ nixos/global/fonts/default.nix
@@ -15,4 +15,16 @@
     ];
   };
 
+  fontProfiles = {
+    enable = true;
+    monospace = {
+      family = "FiraCode Nerd Font";
+      package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
+    };
+    regular = {
+      family = "Fira Sans";
+      package = pkgs.fira;
+    };
+  };
+
 }
