b9211502bd0803857eed2c15a7fe67a2277cade1
Asgaroth <00asgaroth00@gmail.com>
2024-01-19 19:40:06 +0000

    testing

--- nixos/global/fonts/default.nix
+++ nixos/global/fonts/default.nix
@@ -4,6 +4,15 @@
     fontconfig.defaultFonts.monospace = ["Hack Nerd Font Mono"];
     fontDir.enable = true;
     packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
       material-design-icons
       inter
       material-symbols
@@ -11,20 +20,20 @@
       ibm-plex
       nerdfonts
       hack-font
-      (nerdfonts.override {fonts = ["Iosevka" "CascadiaCode" "JetBrainsMono"];})
+      (nerdfonts.override {fonts = ["Iosevka" "CascadiaCode" "JetBrainsMono" "FiraCode" "DroidSansMono"];})
     ];
   };
 
-  fontProfiles = {
-    enable = true;
-    monospace = {
-      family = "FiraCode Nerd Font";
-      package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
-    };
-    regular = {
-      family = "Fira Sans";
-      package = pkgs.fira;
-    };
-  };
+  # fontProfiles = {
+  #   enable = true;
+  #   monospace = {
+  #     family = "FiraCode Nerd Font";
+  #     package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
+  #   };
+  #   regular = {
+  #     family = "Fira Sans";
+  #     package = pkgs.fira;
+  #   };
+  # };
 
 }
