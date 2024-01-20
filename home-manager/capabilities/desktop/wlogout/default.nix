082c9bd14347271941ff5dd5717b40e0316b458d
Asgaroth <00asgaroth00@gmail.com>
2024-01-20 19:28:17 +0000

    testing

--- home-manager/capabilities/desktop/wlogout/default.nix
+++ home-manager/capabilities/desktop/wlogout/default.nix
@@ -1,15 +1,50 @@
 { pkgs, ... }: {
 
-  imports = [
-    ./layout.nix
-  ];
-
-  programs.wlogout.enable = true;
+  # imports = [
+  #   ./layout.nix
+  # ];
 
   home.packages = with pkgs; [
     swaylock-effects
     dunst
   ];
+
+  programs.wlogout = {
+    enable = true;
+    package = pkgs.wlogout;
+    layout = [
+      {
+        label = "lock";
+        action = "sleep 0.4 && swaylock --screenshots --effect-blur 3x5 --fade-in 1 --effect-vignette 0.1:0.8";
+        text = "Lock";
+        keybind = "l";
+      }
+      {
+        label = "logout";
+        action = "hyprctl dispatch exit 0";
+        text = "Logout";
+        keybind = "e";
+      }
+      {
+        label = "reboot";
+        action = "dunstify -a wlogout 'rebooting in 5, please wait' -u 2 && sleep 5 && systemctl reboot";
+        text = "Reboot";
+        keybind = "r";
+      }
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "s";
+      }
+      {
+        label = "suspend";
+        action = "systemctl suspend";
+        text = "󱖒";
+        keybind = "u";
+      }
+    ];
+  };
 
   home.file.".config/hypr/per-app/wlogout.conf" = {
     text = ''
