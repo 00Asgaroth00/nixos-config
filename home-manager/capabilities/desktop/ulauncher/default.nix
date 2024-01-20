f580dafa75af20bb9f5fdc209cfaae1452175919
Asgaroth <00asgaroth00@gmail.com>
2024-01-20 14:03:19 +0000

    testing

--- home-manager/capabilities/desktop/ulauncher/default.nix
+++ home-manager/capabilities/desktop/ulauncher/default.nix
@@ -1,11 +1,9 @@
 { pkgs, home-manager, username, ... }: 
-let
-  hm = home-manager.nixosModules.home-manager.home-manager;
-in {
+{
 
   users.users.${username}.packages = with pkgs; [ ulauncher ];
 
-  hm.users.${username}.home.file.".config/hypr/per-app/ulauncher.conf" = {
+  home.file.".config/hypr/per-app/ulauncher.conf" = {
     text = ''
       exec-once = sleep 1 && ulauncher --hide-window
       windowrulev2 = noborder, class:^(ulauncher)$
