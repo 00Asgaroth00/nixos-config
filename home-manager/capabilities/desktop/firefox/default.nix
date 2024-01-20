77fb70acb089620a4e9d245a0dba7608ab5a5446
Asgaroth <00asgaroth00@gmail.com>
2024-01-20 22:47:18 +0000

    testing

--- home-manager/capabilities/desktop/firefox/default.nix
+++ home-manager/capabilities/desktop/firefox/default.nix
@@ -8,7 +8,7 @@
     enable = true;
     profiles.${username} = {
       bookmarks = {};
-      extensions = with inputs.firefox-addons.packages; [
+      extensions = with inputs.firefox-addons; [
         ublock-origin
       ];
       settings = {
