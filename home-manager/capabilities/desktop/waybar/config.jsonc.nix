6041980941c697762d9a0bf555990b6467ba64fc
Asgaroth <00asgaroth00@gmail.com>
2024-01-20 15:12:21 +0000

    testing

--- home-manager/capabilities/desktop/waybar/config.jsonc.nix
+++ home-manager/capabilities/desktop/waybar/config.jsonc.nix
@@ -104,7 +104,7 @@
           ],
           "on-scroll-up": "brightnessctl set +1%",
           "on-scroll-down": "brightnessctl set 1%-",
-          "on-click": "brightnessctl set 0",
+          "on-click": "brightnessctl set 50",
           "tooltip": false
         },
 
