c9b62dbfcca0f04307275ab6f4daca54ab595199
Asgaroth <00asgaroth00@gmail.com>
2024-01-20 21:20:51 +0000

    testing

--- home-manager/capabilities/kitty/default.nix
+++ home-manager/capabilities/kitty/default.nix
@@ -36,18 +36,30 @@
     };
   };
 
-  home.file.".config/hypr/per-app/kitty.conf" = {
-    text = ''
-      windowrulev2 = workspace 1,class:^(kitty)$
-      windowrulev2 = opacity 0.8 0.8, class:^(kitty)$
-      # windowrulev2 = size 700 300, class:^(kitty)$
-      # windowrulev2 = center, class:^(kitty)$
-      # TODO make above a single line :)
-      # bind = $mainMod, Q, exec, kitty
-      bind = ALT, Return, exec, kitty
-      # bind = control, escape, exec, kitty -e btm
-      # windowrule = float, title:zsh
-    '';
-  };
+  # home.file.".config/hypr/per-app/kitty.conf" = {
+  #   text = ''
+  #     windowrulev2 = workspace 1,class:^(kitty)$
+  #     windowrulev2 = opacity 0.8 0.8, class:^(kitty)$
+  #     # windowrulev2 = size 700 300, class:^(kitty)$
+  #     # windowrulev2 = center, class:^(kitty)$
+  #     # TODO make above a single line :)
+  #     # bind = $mainMod, Q, exec, kitty
+  #     bind = ALT, Return, exec, kitty
+  #     # bind = control, escape, exec, kitty -e btm
+  #     # windowrule = float, title:zsh
+  #   '';
+  # };
+
+  xdg.configFile."hypr/per-app/kitty.conf".text = ''
+    windowrulev2 = workspace 1,class:^(kitty)$
+    windowrulev2 = opacity 0.8 0.8, class:^(kitty)$
+    # windowrulev2 = size 700 300, class:^(kitty)$
+    # windowrulev2 = center, class:^(kitty)$
+    # TODO make above a single line :)
+    # bind = $mainMod, Q, exec, kitty
+    bind = ALT, Return, exec, kitty
+    # bind = control, escape, exec, kitty -e btm
+    # windowrule = float, title:zsh
+  '';
 
 }
