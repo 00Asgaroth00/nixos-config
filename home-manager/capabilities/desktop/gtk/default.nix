3a3ca8ee28a68466862e9479d09aae5c840a0102
Asgaroth <00asgaroth00@gmail.com>
2024-01-20 16:53:50 +0000

    testing

--- home-manager/capabilities/desktop/gtk/default.nix
+++ home-manager/capabilities/desktop/gtk/default.nix
@@ -1,15 +1,19 @@
 { lib, pkgs, ... }: {
 
+  home.packages = with pkgs; [
+    catppuccin-gtk
+  ];
+
   gtk = {
     enable = true;
     theme = {
       name = "Catppuccin-Macchiato-Compact-Pink-Dark";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "pink" ];
-        size = "compact";
-        tweaks = [ "rimless" "black" ];
-        variant = "macchiato";
-      };
+      # package = pkgs.catppuccin-gtk.override {
+      #   accents = [ "pink" ];
+      #   size = "compact";
+      #   tweaks = [ "rimless" "black" ];
+      #   variant = "macchiato";
+      # };
     };
   };
 
