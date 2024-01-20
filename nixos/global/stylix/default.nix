0dd6b545d229ccca10cd1be9dda6d2367796ba61
Asgaroth <00asgaroth00@gmail.com>
2024-01-20 11:06:14 +0000

    testing

--- nixos/global/stylix/default.nix
+++ nixos/global/stylix/default.nix
@@ -3,9 +3,9 @@
   stylix = {
     base16Scheme = "${pkgs.base16-schemes}/share/themes/${colour_scheme}.yaml";
     
-    image = pkgs.fetchurl {
-      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+    wallpaper = config.lib.stylix.mkAnimation {
+      animation = ../../../home-manager/capabilities/desktop/wallpapers/lofi-cafe.gif;
+      polarity = "dark";
     };
     
     fonts = {
