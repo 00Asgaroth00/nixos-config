f6c72f5791af5667a3ea2626774e367d5af8ebf2
Asgaroth <00asgaroth00@gmail.com>
2024-01-20 17:21:02 +0000

    testing

--- home-manager/capabilities/desktop/gtk/default.nix
+++ home-manager/capabilities/desktop/gtk/default.nix
@@ -1,11 +1,18 @@
 { lib, pkgs, ... }: {
 
-  home.packages = with pkgs; [
-    catppuccin-gtk
-  ];
+  # home.packages = with pkgs; [
+  #   catppuccin-gtk
+  # ];
 
   gtk = {
     enable = true;
+    font = {
+      size = 12;
+    };
+    iconThem = {
+      name = "Papirus";
+      package = pkgs.papirus-icon-theme;
+    }
     theme = {
       name = lib.mkDefault "Catppuccin-Macchiato-Compact-Pink-Dark";
       # package = pkgs.catppuccin-gtk.override {
