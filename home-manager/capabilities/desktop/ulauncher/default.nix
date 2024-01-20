3abb4ea9501f521cc2b1ff885dff347d722524bd
Asgaroth <00asgaroth00@gmail.com>
2024-01-20 14:04:38 +0000

    testing

--- home-manager/capabilities/desktop/ulauncher/default.nix
+++ home-manager/capabilities/desktop/ulauncher/default.nix
@@ -1,9 +1,9 @@
-{ pkgs, home-manager, username, ... }: 
+{ pkgs, username, ... }: 
 {
 
   users.users.${username}.packages = with pkgs; [ ulauncher ];
 
-  home.file.".config/hypr/per-app/ulauncher.conf" = {
+  home-manager.users.${username}.home.file.".config/hypr/per-app/ulauncher.conf" = {
     text = ''
       exec-once = sleep 1 && ulauncher --hide-window
       windowrulev2 = noborder, class:^(ulauncher)$
