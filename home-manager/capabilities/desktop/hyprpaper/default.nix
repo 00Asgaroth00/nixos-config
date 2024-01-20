c4eb8e9f35da9cfe07c0fc302e64bb5c0ea6aa61
Asgaroth <00asgaroth00@gmail.com>
2024-01-20 12:00:16 +0000

    testing

---
+++ home-manager/capabilities/desktop/hyprpaper/default.nix
@@ -0,0 +1,11 @@
+{ config, username ... }: {
+
+  home-manager.user.${username}.home.file.".config/hypr/hyprpaper.conf" = {
+    text = ''
+      preload = ~/git_repos/nixos-config/home-manager/capabilities/desktop/wallpapers/lofi-cafe.gif
+
+      wallpaper = eDP-1, ~/git_repos/nixos-config/home-manager/capabilities/desktop/wallpapers/lofi-cafe.gif
+    '';
+  };
+
+}
