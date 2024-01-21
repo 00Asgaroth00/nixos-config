54d0177e8de01151089fa93810755c1c39b5d62a
Asgaroth <00asgaroth00@gmail.com>
2024-01-21 11:32:26 +0000

    testing

--- home-manager/capabilities/cli/default.nix
+++ home-manager/capabilities/cli/default.nix
@@ -14,11 +14,20 @@
   home.packages = with pkgs; [
     entr        # run arbitrary commands when files change
     fd          # enhanced alternative to find
-    ipfetch     # neofetch like tool that can lookup ips
+    ipfetch     # neofetch like tool that can lookup ips (giving errors: IP is invalid or API is down)
     miller      # awk/sed/cut/join/sort for data formats like csv/tsv/json
     neofetch    # highly customizable system info script
     ripgrep     # enhanced alternative to grep
     toilet      # display colourfull characters in text mode
+
+    # worky stuff
+    iproute2    # ip utils, should be installed by default, but make sure
+    dig         # dns toolset
+    ngrep       # network packet analyzer
+    sngrep      # voip message flows in terminal
+    sipp        # sip testing tool
+    sipsak      # sip swiss army knife
+    sipexer     # modern sip cli tool
   ];
 
 }
