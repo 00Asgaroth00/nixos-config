47e8c10bf2c7ece881de28ef1dcba2aaf3c1bf4d
Asgaroth <00asgaroth00@gmail.com>
2024-01-21 09:43:02 +0000

    testing

--- home-manager/capabilities/cli/starship/default.nix
+++ home-manager/capabilities/cli/starship/default.nix
@@ -50,17 +50,17 @@
         symbol = " ";
         style = "bold red";
       };
-      custom = {
-        nix_inspect = let
-          excluded = [ "kitty" "imagemagick" "ncurses" "user-environment" ];
-        in {
-          disabled = false;
-          when = "test -z $IN_NIX_SHELL";
-          command = "${(lib.getExe pkgs.nix-inspect)} ${(lib.concatStringsSep " " excluded)}";
-          format = "[($output <- )$symbol]($style) ";
-          symbol = " ";
-          style = "bold blue";
-        };
+      # custom = {
+      #   nix_inspect = let
+      #     excluded = [ "kitty" "imagemagick" "ncurses" "user-environment" ];
+      #   in {
+      #     disabled = false;
+      #     when = "test -z $IN_NIX_SHELL";
+      #     command = "${(lib.getExe pkgs.nix-inspect)} ${(lib.concatStringsSep " " excluded)}";
+      #     format = "[($output <- )$symbol]($style) ";
+      #     symbol = " ";
+      #     style = "bold blue";
+      #   };
       };
 
       character = {
