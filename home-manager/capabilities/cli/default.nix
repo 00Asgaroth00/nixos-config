12f5eaa3ad67f79364dfa893a1e000f182493867
Asgaroth <00asgaroth00@gmail.com>
2024-01-21 09:31:48 +0000

    testing

--- home-manager/capabilities/cli/default.nix
+++ home-manager/capabilities/cli/default.nix
@@ -2,6 +2,7 @@
 
   imports = [
     ./bat
+    ./starship
     ./zsh
   ];
 
--- home-manager/capabilities/shell.nix
+++ home-manager/capabilities/shell.nix
@@ -45,83 +45,83 @@
   #   };
   # };
 
-  programs.zsh = {
-    enable = true;
-    enableAutosuggestions = true;
-    enableCompletion = true;
-    autocd = true;
-    cdpath = [ "." "/home/${username}" "/home/${username}/Public" "/home/${username}/Documents" "/home/${username}/Notes" ];
-    defaultKeymap = "viins";
-    dotDir = ".config/zsh";
-    plugins = [
-      { name = "fzf-tab"; src = "${pkgs.zsh-fzf-tab}/share/fzf-tab"; }
-    ];
-    shellGlobalAliases = {
-      E = "| entr -c";
-      F = "| fzf";
-      R = "| rg";
-    };
-    shellAliases = {
-      t = "task";
-      cat = "bat --paging=never";
-      mv = "mv -iv";
-      cp = "cp -iv";
-      rm = "rm -v";
-      v = "nvim";
-      m = "iex -S mix";
-      ms = "iex -S mix phx.server";
-      mdg = "mix deps.get";
-      mdc = "mix deps.clean --all";
-    };
-    history = {
-      path = "${config.xdg.dataHome}/zsh/zsh_history";
-      expireDuplicatesFirst = true;
-      ignoreDups = true;
-      extended = true;
-      ignoreSpace = true;
-    };
-    envExtra = ''
-      export MANPAGER="nvim +Man!"
-      export PAGER=bat
-      BLK="00" CHAR="00" DIR="0C" EXE="DE" REG="00" HLI="00" SLI="00" MIS="00" ORP="00" FIF="00" SOC="00" UNK="00"
-      export NNN_FCOLORS="$BLK$CHAR$DIR$EXE$REG$HLI$SLI$MIS$ORP$FIF$SOC$UNK"
-      export NNN_COLORS="6666"
-      export NNN_OPTS=Hd
-      export NNN_FIFO=/tmp/nnn.fifo
-    '';
+  # programs.zsh = {
+  #   enable = true;
+  #   enableAutosuggestions = true;
+  #   enableCompletion = true;
+  #   autocd = true;
+  #   cdpath = [ "." "/home/${username}" "/home/${username}/Public" "/home/${username}/Documents" "/home/${username}/Notes" ];
+  #   defaultKeymap = "viins";
+  #   dotDir = ".config/zsh";
+  #   plugins = [
+  #     { name = "fzf-tab"; src = "${pkgs.zsh-fzf-tab}/share/fzf-tab"; }
+  #   ];
+  #   shellGlobalAliases = {
+  #     E = "| entr -c";
+  #     F = "| fzf";
+  #     R = "| rg";
+  #   };
+  #   shellAliases = {
+  #     t = "task";
+  #     cat = "bat --paging=never";
+  #     mv = "mv -iv";
+  #     cp = "cp -iv";
+  #     rm = "rm -v";
+  #     v = "nvim";
+  #     m = "iex -S mix";
+  #     ms = "iex -S mix phx.server";
+  #     mdg = "mix deps.get";
+  #     mdc = "mix deps.clean --all";
+  #   };
+  #   history = {
+  #     path = "${config.xdg.dataHome}/zsh/zsh_history";
+  #     expireDuplicatesFirst = true;
+  #     ignoreDups = true;
+  #     extended = true;
+  #     ignoreSpace = true;
+  #   };
+  #   envExtra = ''
+  #     export MANPAGER="nvim +Man!"
+  #     export PAGER=bat
+  #     BLK="00" CHAR="00" DIR="0C" EXE="DE" REG="00" HLI="00" SLI="00" MIS="00" ORP="00" FIF="00" SOC="00" UNK="00"
+  #     export NNN_FCOLORS="$BLK$CHAR$DIR$EXE$REG$HLI$SLI$MIS$ORP$FIF$SOC$UNK"
+  #     export NNN_COLORS="6666"
+  #     export NNN_OPTS=Hd
+  #     export NNN_FIFO=/tmp/nnn.fifo
+  #   '';
 
-    # how to see where a package ends up in the store
-    # source ${pkgs.asdf-vm.outPath}/bin/asdf
+  #   # how to see where a package ends up in the store
+  #   # source ${pkgs.asdf-vm.outPath}/bin/asdf
 
-    initExtraBeforeCompInit = ''
-      setopt AUTO_PUSHD           # Push the old directory onto the stack on cd
-      setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack
-      setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd
-      setopt CORRECT              # Spelling Corrections
-      setopt CDABLE_VARS          # Change directory to a path stored in a variable
-      setopt EXTENDED_GLOB        # Use extended globbing syntax
-      KEYTIMEOUT=5
-    '';
-    initExtra = ''
-      autoload -Uz edit-command-line
-      zle -N edit-command-line
-      bindkey -M viins '^f' edit-command-line
-      bindkey -M vicmd '^i' edit-command-line
+  #   initExtraBeforeCompInit = ''
+  #     setopt AUTO_PUSHD           # Push the old directory onto the stack on cd
+  #     setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack
+  #     setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd
+  #     setopt CORRECT              # Spelling Corrections
+  #     setopt CDABLE_VARS          # Change directory to a path stored in a variable
+  #     setopt EXTENDED_GLOB        # Use extended globbing syntax
+  #     KEYTIMEOUT=5
+  #   '';
+  #   initExtra = ''
+  #     autoload -Uz edit-command-line
+  #     zle -N edit-command-line
+  #     bindkey -M viins '^f' edit-command-line
+  #     bindkey -M vicmd '^i' edit-command-line
 
-      source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+  #     source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
     
-      color=$(( ( RANDOM % 6 ) + 1 ))
-      tput setaf $color && ${pkgs.toilet}/bin/toilet -F border -t -f pagga "Bit by Bit"
+  #     color=$(( ( RANDOM % 6 ) + 1 ))
+  #     tput setaf $color && ${pkgs.toilet}/bin/toilet -F border -t -f pagga "Bit by Bit"
 
-      function w() {
-        fd $1 | entr -c "''${@:2}"
-      }
+  #     function w() {
+  #       fd $1 | entr -c "''${@:2}"
+  #     }
 
-      function ew() {
-        fd "\.exs?$" | entr -c "$@"
-      }
-    '';
-  };
+  #     function ew() {
+  #       fd "\.exs?$" | entr -c "$@"
+  #     }
+  #   '';
+  # };
 
   programs.starship = {
     enable = true;
