9a9b96bade512d03f1d25ec603b5bd2324e3f9e5
Asgaroth <00asgaroth00@gmail.com>
2024-01-21 18:48:21 +0000

    testing

--- home-manager/capabilities/common/xdg.nix
+++ home-manager/capabilities/common/xdg.nix
@@ -7,7 +7,13 @@
       enable = true;
       createDirectories = true;
       extraConfig = {
+        XDG_GIT_DIR = "${config.home.homeDirectory}/Git";
+        XDG_WORK_DIR = "${config.home.homeDirectory}/Work";
         XDG_WORK_DOCUMENTS_DIR = "${config.home.homeDirectory}/Work/Documents";
+        XDG_WORK_DOWNLOAD_DIR = "${config.home.homeDirectory}/Work/Downloads";
+        XDG_WORK_PICTURES_DIR = "${config.home.homeDirectory}/Work/Pictures";
+        XDG_WORK_VIDEOS_DIR = "${config.home.homeDirectory}/Work/Videos";
+        XDG_WORK_GIT_DIR = "${config.home.homeDirectory}/Work/Git";
       };
     };
 
