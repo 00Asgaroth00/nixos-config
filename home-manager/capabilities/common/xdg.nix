48982b09ec6c65dbd776cef4800ce031998a169d
Asgaroth <00asgaroth00@gmail.com>
2024-01-21 18:39:42 +0000

    testing

--- home-manager/capabilities/common/xdg.nix
+++ home-manager/capabilities/common/xdg.nix
@@ -6,6 +6,9 @@
     userDirs = {
       enable = true;
       createDirectories = true;
+      extraConfig = {
+        XDG_WORK_DOCUMENTS_DIR = "config.home.homeDirectory}/Work/Documents";
+      };
     };
 
   };
