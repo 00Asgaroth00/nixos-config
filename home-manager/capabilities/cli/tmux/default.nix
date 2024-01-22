ddaa52c25550f54471efe19c052af6a0aa8aef2e
Asgaroth <00asgaroth00@gmail.com>
2024-01-22 16:46:00 +0000

    testing

--- home-manager/capabilities/cli/tmux/default.nix
+++ home-manager/capabilities/cli/tmux/default.nix
@@ -2,14 +2,14 @@
 
   programs.tmux = {
     enable = true;
-    # package = with pkgs; [ tmux ];
+    package = pkgs.tmux;
     keyMode = "vi";
     baseIndex = 1;
     clock24 = true;
     mouse = true;
     historyLimit = 10000;
     secureSocket = true;
-    shell = "\${pkgs.zsh}/bin/zsh";
+    shell = "${pkgs.zsh}/bin/zsh";
     terminal = "xterm-256color";
     extraConfig = ''
       set -ga terminal-overrides ",xterm-256color:Tc"
