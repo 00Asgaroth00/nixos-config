f46a0e279a46888dd3dd1c836e102a4830e000a3
Asgaroth <00asgaroth00@gmail.com>
2024-01-22 17:27:13 +0000

    testing

--- home-manager/capabilities/cli/tmux/default.nix
+++ home-manager/capabilities/cli/tmux/default.nix
@@ -1,8 +1,22 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: 
 
   # https://github.com/tmux-plugins/tmux-sensible
   # https://github.com/tmux-plugins/tmux-pain-control
 
+  let
+    tmux-toko-night = pkgs.tmuxPlugins.mkTmuxPlugin {
+      pluginName = "tmux-tokyo-night";
+      version = "unstable-2024-01-22";
+      rtpFilePath = "tmux-tokyo-night.tmux";
+      src = pkgs.fetchFromGitHub {
+        owner = "fabioluciano";
+        repo = "tmux-tokyo-night";
+        rev = "ee73d4a9ba6222d7d51492a4e0e797c9249a879c";
+        # sha256 = "sha256-HN0hJeB31MvkD12dbnF2SjefkAVgtUmhah598zAlhQs=";
+      };
+    };
+
+  in {
   programs.tmux = {
     enable = true;
     package = pkgs.tmux;
@@ -18,6 +32,14 @@
     plugins = with pkgs.tmuxPlugins; [
       pain-control
       better-mouse-mode
+      {
+        plugin = tmux-tokyo-night;
+        extraConfig = ''
+          set -g @theme_variation 'moon'
+          set -g @theme_left_separator ''
+          set -g @theme_right_separator ''
+        '';
+      }
     ];
     extraConfig = ''
       set -ga terminal-overrides ",xterm-256color:Tc"
