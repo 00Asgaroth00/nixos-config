00944ff9bd8778ed971ad768116132aa2975c4bb
Asgaroth <00asgaroth00@gmail.com>
2024-01-22 11:44:21 +0000

    testing

--- home-manager/capabilities/cli/neovim/default.nix
+++ home-manager/capabilities/cli/neovim/default.nix
@@ -16,11 +16,20 @@
     
     plugins = with pkgs.vimPlugins; [
       {
-        plugin = tokyonight-nvim;
+        plugin = tokyonight-nvim; # TokyoNight Colours
         type = "lua";
         config = ''
           require("tokyonight").setup{}
           vim.cmd[[colorscheme tokyonight-night]]
+        '';
+      }
+      {
+        plugin = nvim-treesitter.withAllGrammars; # Syntax Highlighting
+        type = "lua";
+        config = ''
+          require('nvim-treesitter.configs').setup {
+            highlight = { enable = true}
+          }
         '';
       }
     ];
