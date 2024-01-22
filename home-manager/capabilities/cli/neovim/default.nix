80409e95f41f2f009281acdfece45e5b07853fac
Asgaroth <00asgaroth00@gmail.com>
2024-01-22 10:44:56 +0000

    testing

--- home-manager/capabilities/cli/neovim/default.nix
+++ home-manager/capabilities/cli/neovim/default.nix
@@ -7,10 +7,14 @@
   # };
 
   programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
+    # enable = true;
+    # defaultEditor = true;
+    # viAlias = true;
+    # vimAlias = true;
+    
+    plugins = with pkgs.vimPlugins; = [
+      tokyonight-nvim
+    ];
   };
 
 }
