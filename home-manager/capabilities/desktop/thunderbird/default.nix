1c7794af9d3d13ed63e5887d8f60b4d2c0541362
Asgaroth <00asgaroth00@gmail.com>
2024-01-23 20:15:52 +0000

    testing

---
+++ home-manager/capabilities/desktop/thunderbird/default.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }: {
+
+  accounts.email.accounts = {
+    Asgaroth = {
+      primary = false;
+      username = "00asgaroth00@gmail.com";
+      realName = "Asgaroth";
+      address = "00asgaroth00@gmail.com";
+      flavor = "gmail.com";
+      thunderbird.enable = true;
+      thunderbird.profiles = [ "home" ];
+    };
+    VoidBurn = {
+      primary = false;
+      username = "voidburn@hotmail.com";
+      realName = "VoidBurn";
+      address = "voidburn@hotmail.com";
+      flavor = "outlook.office365.com";
+      thunderbird.enable = true;
+      thunderbird.profiles = [ "home" ];
+    };
+  };
+
+  programs.thunderbird = {
+    enable = true;
+    profiles = {
+      home.isDefault = true;
+    }
+  };
+
+  xdg.configFile."hypr/per-app/thunderbird.conf".text = ''
+    bind = $mainMod, T, exec, thunderbird
+    # bind = $mainMod, W, exec, firefox -p work
+    exec-once = [workspace 3 silent] thunderbird
+  '';
+
+}
