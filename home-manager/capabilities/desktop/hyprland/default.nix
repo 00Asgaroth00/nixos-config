5542799492fe1920dbcaf9f430fce637b64016c8
Asgaroth <00asgaroth00@gmail.com>
2024-01-24 21:07:52 +0000

    testing

--- home-manager/capabilities/desktop/hyprland/default.nix
+++ home-manager/capabilities/desktop/hyprland/default.nix
@@ -257,7 +257,13 @@ in {
     };
   };
 
+  home.sessionVariables = {
+    BEMOJI_PICKER_CMD = "${pkgs.wofi}/bin/wofi";
+    BEMOJI_CLIP_CMD = "${pkgs.wl-clipboard}/bin/wl-copy";
+    BEMOJI_TYPE_CMD = "${pkgs.wtype}/bin/wtype";
+  };
+  
   xdg.configFile."hypr/per-app/bemoji.conf".text = ''
-    bind = "ALT, e, exec, bemoji -t"
+    bind = ALT, e, exec, ${pkgs.bemoji}/bin/bemoji -t
   '';
 }
