4f9e51e020856af3d2128760ba8780529d849927
Asgaroth <00asgaroth00@gmail.com>
2024-01-24 22:45:06 +0000

    testing

--- home-manager/capabilities/desktop/playerctl/default.nix
+++ home-manager/capabilities/desktop/playerctl/default.nix
@@ -10,6 +10,7 @@
     bind = , XF86AudioPrev, exec, ${pkgs.playerctl}/bin/playerctl previous
     bind = , XF86AudioNext, exec, ${pkgs.playerctl}/bin/playerctl next
     bind = , XF86AudioPlay, exec, ${pkgs.playerctl}/bin/playerctl play-pause
+    bind = , XF86AudioPause, exec, ${pkgs.playerctl}/bin/playerctl play-pause
   '';
 
 }
