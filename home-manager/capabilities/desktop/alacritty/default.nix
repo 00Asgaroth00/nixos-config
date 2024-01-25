6904e07cbedf24f87e0a8ac9788c6e26c5b00f06
Asgaroth <00asgaroth00@gmail.com>
2024-01-25 22:34:01 +0000

    testing

--- home-manager/capabilities/desktop/alacritty/default.nix
+++ home-manager/capabilities/desktop/alacritty/default.nix
@@ -9,4 +9,16 @@
     package = pkgs.alacritty;
   };
 
+  xdg.configFile."hypr/per-app/alacritty.conf".text = ''
+    windowrulev2 = workspace 1,class:^(alacritty)$
+    windowrulev2 = opacity 0.8 0.8, class:^(alacritty)$
+    # windowrulev2 = size 700 300, class:^(kitty)$
+    # windowrulev2 = center, class:^(kitty)$
+    # TODO make above a single line :)
+    # bind = $mainMod, Q, exec, kitty
+    bind = ALT, Return, exec, kitty
+    # bind = control, escape, exec, kitty -e btm
+    # windowrule = float, title:zsh
+  '';
+
 }
