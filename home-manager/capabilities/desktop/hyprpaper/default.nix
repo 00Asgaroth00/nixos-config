9957647d38e3ac15ab7a4f9930c2a59af3540ba4
Asgaroth <00asgaroth00@gmail.com>
2024-01-26 20:53:41 +0000

    testing

--- home-manager/capabilities/desktop/hyprpaper/default.nix
+++ home-manager/capabilities/desktop/hyprpaper/default.nix
@@ -1,21 +1,17 @@
-{ config, pkgs, ... }: {
-
-  import = [
-    ./${config.networking.hostName}.nix
-  ]
+{
+  osConfig,
+  pkgs,
+  ...
+}: {
+  imports = [
+    (./. + "/${osConfig.networking.hostName}.nix")
+  ];
 
   # wallpaper manager
 
-  home.packages = with pkgs; [ hyprpaper ];
+  home.packages = with pkgs; [hyprpaper];
 
   xdg.configFile."hypr/per-app/hyprpaper.conf".text = ''
     exec-once = ${pkgs.hyprpaper}/bin/hyprpaper
   '';
-
-
-  # xdg.configFile."hypr/hyprpaper.conf".text = ''
-  #   preload = ~/Git/nixos-config/home-manager/capabilities/desktop/wallpapers/zi2Bbda-anime-dragon-wallpaper.jpg
-  #   wallpaper = eDP-1, ~/Git/nixos-config/home-manager/capabilities/desktop/wallpapers/zi2Bbda-anime-dragon-wallpaper.jpg
-  # '';
-
 }
