50339b73f7baeeb1cd2d4297c30e4a137df2f483
Asgaroth <00asgaroth00@gmail.com>
2024-01-27 16:36:57 +0000

    testing

--- nixos/global/greetd/default.nix
+++ nixos/global/greetd/default.nix
@@ -2,16 +2,16 @@
 
   # See https://www.reddit.com/r/NixOS/comments/14rhsnu/regreet_greeter_for_greetd_doesnt_show_a_session/
 
-  services.xserver.displayManager.session = [
-    {
-      manage = "desktop";
-      name = "hyprland";
-      start = ''
-        ${lib.getExe pkgs.hyprland} &
-        waitPID=$!
-      '';
-    }
-  ];
+  # services.xserver.displayManager.session = [
+  #   {
+  #     manage = "desktop";
+  #     name = "hyprland";
+  #     start = ''
+  #       ${lib.getExe pkgs.hyprland} &
+  #       waitPID=$!
+  #     '';
+  #   }
+  # ];
 
   services.greetd.enable = true;
 
