b7394f892478f6b2816337883ca4447fa2c01db4
Asgaroth <00asgaroth00@gmail.com>
2024-01-28 23:29:08 +0000

    testing

--- nixos/common/hyprland/default.nix
+++ nixos/common/hyprland/default.nix
@@ -7,7 +7,7 @@
   programs.hyprland = {
     enable = true;
     # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    packages = pkgs.hyprland;
+    package = pkgs.hyprland;
   };
 
   xdg = {
