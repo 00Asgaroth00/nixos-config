a17481391cdd669c195f62a71017ab95e382d80d
Asgaroth <00asgaroth00@gmail.com>
2024-01-28 07:51:34 +0000

    testng

--- home-manager/capabilities/desktop/hyprpaper/arakasi.nix
+++ home-manager/capabilities/desktop/hyprpaper/arakasi.nix
@@ -1,4 +1,8 @@
 {...}: {
+
+  # see this for an alternative implementation 
+  #    https://discord.com/channels/568306982717751326/741347063077535874/1125784739618496562
+
   xdg.configFile."hypr/hyprpaper.conf".text = ''
     preload = ~/Git/nixos-config/home-manager/capabilities/desktop/wallpapers/zi2Bbda-anime-dragon-wallpaper.jpg
     wallpaper = HDMI-A-1, ~/Git/nixos-config/home-manager/capabilities/desktop/wallpapers/zi2Bbda-anime-dragon-wallpaper.jpg
