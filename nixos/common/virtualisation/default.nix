4292c7e7f870cd49ef24d3511b75c4e86ef288d8
Asgaroth <00asgaroth00@gmail.com>
2024-01-28 22:05:24 +0000

    testing

--- nixos/common/virtualisation/default.nix
+++ nixos/common/virtualisation/default.nix
@@ -15,14 +15,10 @@
     # extraPackages = with pkgs; [
     #   docker-buildx
     #   docker-compose
-
-    #   # nvidia-docker # inject this for main workstation, dont have nvidia gpu on laptop
     # ];
     extraPackages = lib.mkMerge [
       [pkgs.docker-buildx pkgs.docker-compose]
       (lib.mkIf (config.networking.hostName == "arakasi") [pkgs.nvidia-docker])
     ];
   };
-
-  # virtualisation.docker.extraPackages = lib.mkIf (config.networking.hostName == "arakasi") lib.mkMerge [pkgs.nvidia-docker];
 }
