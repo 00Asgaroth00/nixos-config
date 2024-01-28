94e9b3d124032914c203558e633649340f64cb17
Asgaroth <00asgaroth00@gmail.com>
2024-01-28 20:31:34 +0000

    testing

--- nixos/common/users/vzhsxn/default.nix
+++ nixos/common/users/vzhsxn/default.nix
@@ -3,7 +3,7 @@
   username,
   ...
 }: {
-  users.user.${username} = {
+  users.users.${username} = {
     # TODO: You can set an initial password for your user.
     # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
     # Be sure to change it (using passwd) after rebooting!
