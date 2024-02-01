2cd755a7b890094c27e0376475a2d74b479d38b1
Asgaroth <00asgaroth00@gmail.com>
2024-02-01 23:16:17 +0000

    testing

--- home-manager/capabilities/desktop/microsoft-edge/default.nix
+++ home-manager/capabilities/desktop/microsoft-edge/default.nix
@@ -3,8 +3,8 @@
     microsoft-edge-beta # ms edge browser
   ];
 
-  xdg.configFile."hypr/per-app/firefox.conf".text = ''
-    bind = $mainMod, /, exec, ${pkgs.microsoft-edge-beta}/opt/microsoft/msedge-beta/microsoft-edge
+  xdg.configFile."hypr/per-app/microsoft-edge.conf".text = ''
+    bind = $mainMod, M, exec, ${pkgs.microsoft-edge-beta}/opt/microsoft/msedge-beta/microsoft-edge
     exec-once = [workspace 2 silent] ${pkgs.microsoft-edge-beta}/opt/microsoft/msedge-beta/microsoft-edge
   '';
 }
