ba901390519b516ad610008178e4f37a39d68170
Asgaroth <00asgaroth00@gmail.com>
2024-02-02 14:49:31 +0000

    testing

--- home-manager/capabilities/desktop/hyprland/arakasi.nix
+++ home-manager/capabilities/desktop/hyprland/arakasi.nix
@@ -6,6 +6,9 @@
       "GBM_BACKEND,nvidia-drm"
       "__GLX_VENDOR_LIBRARY_NAME,nvidia"
       "WLR_NO_HARDWARE_CURSORS,1"
+      "MOZ_DISABLE_RDD_SANDBOX,1"
+      "NVD_BACKEND,direct"
+      "EGL_PLATFORM,wayland";
     ];
   };
 }
