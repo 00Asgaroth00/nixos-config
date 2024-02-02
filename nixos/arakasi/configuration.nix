039fdd510610e10ae7edf78e85135413d7e0c2b2
Asgaroth <00asgaroth00@gmail.com>
2024-02-02 15:51:19 +0000

    testing

--- nixos/arakasi/configuration.nix
+++ nixos/arakasi/configuration.nix
@@ -135,7 +135,7 @@ in {
       driSupport = true;
       driSupport32Bit = true;
       extraPackages = with pkgs; [
-        nvidia-vaapi-driver
+        # nvidia-vaapi-driver
         libGL
         # libvdpau-va-gl
         vulkan-tools
