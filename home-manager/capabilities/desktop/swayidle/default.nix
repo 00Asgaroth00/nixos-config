e8b2d4ab046cb9cdc0b1a09ccebe540b8c29ab78
Asgaroth <00asgaroth00@gmail.com>
2024-02-11 15:37:00 +0000

    testing

--- home-manager/capabilities/desktop/swayidle/default.nix
+++ home-manager/capabilities/desktop/swayidle/default.nix
@@ -14,11 +14,11 @@
     ];
     timeouts = [
       {
-        timeout = 300; # Lock screens after 5 minutes
+        timeout = 180; # Lock screens after 3 minutes
         command = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --effect-blur 3x5 --fade-in 1 --effect-vignette 0.1:0.8";
       }
       {
-        timeout = 360; # Turn off screens after 6 minutes
+        timeout = 300; # Turn off screens after 5 minutes
         command = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
         resumeCommand = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
       }
