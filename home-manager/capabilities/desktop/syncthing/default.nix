f8eb43064e0ab97ebb689bbd982a674f6cdd106a
Asgaroth <00asgaroth00@gmail.com>
2024-02-11 22:32:46 +0000

    testing

--- home-manager/capabilities/desktop/syncthing/default.nix
+++ home-manager/capabilities/desktop/syncthing/default.nix
@@ -1,8 +1,20 @@
-{...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   services.syncthing = {
     enable = true;
     tray = {
       enable = true;
+    };
+  };
+
+  # tray starts before hyprland tray is available
+  # see: https://github.com/nix-community/home-manager/issues/3416
+  systemd.user.services = {
+    syncthingtray.Service = {
+      ExecStart = lib.mkForce "${pkgs.syncthingtray}/bin/syncthingtray --wait";
     };
   };
 }
