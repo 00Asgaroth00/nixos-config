427685032c258e4e25fc34d9d9f874df28f98f3b
Asgaroth <00asgaroth00@gmail.com>
2024-02-11 15:42:24 +0000

    testing

--- home-manager/capabilities/desktop/swayidle/default.nix
+++ home-manager/capabilities/desktop/swayidle/default.nix
@@ -5,17 +5,17 @@
     events = [
       {
         event = "lock";
-        command = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --effect-blur 3x5 --fade-in 1 --effect-vignette 0.1:0.8";
+        command = "${pkgs.swaylock-effects}/bin/swaylock --daemonize --screenshots --effect-blur 3x5 --fade-in 1 --effect-vignette 0.1:0.8";
       }
       {
         event = "before-sleep";
-        command = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --effect-blur 3x5 --fade-in 1 --effect-vignette 0.1:0.8";
+        command = "${pkgs.swaylock-effects}/bin/swaylock --daemonize --screenshots --effect-blur 3x5 --fade-in 1 --effect-vignette 0.1:0.8";
       }
     ];
     timeouts = [
       {
         timeout = 180; # Lock screens after 3 minutes
-        command = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --effect-blur 3x5 --fade-in 1 --effect-vignette 0.1:0.8";
+        command = "${pkgs.swaylock-effects}/bin/swaylock --daemonize --screenshots --effect-blur 3x5 --fade-in 1 --effect-vignette 0.1:0.8";
       }
       {
         timeout = 300; # Turn off screens after 5 minutes
