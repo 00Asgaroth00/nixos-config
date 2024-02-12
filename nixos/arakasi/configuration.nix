c05052c458de305c577dc776220ddc06e38b631e
Asgaroth <00asgaroth00@gmail.com>
2024-02-12 13:54:30 +0000

    testing

--- nixos/arakasi/configuration.nix
+++ nixos/arakasi/configuration.nix
@@ -172,7 +172,7 @@ in {
   };
 
   powerManagement = {
-    cpuFreqGovernor = "schedutil"; # ondemand/powersave/performance/schedutil
+    cpuFreqGovernor = "performance"; # ondemand/powersave/performance/schedutil
   };
 
   # fonts.fontDir.enable = true;
