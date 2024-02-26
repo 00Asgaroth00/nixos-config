a43e8df01c5f01496cf5ac0e75cf442d547ad85a
Asgaroth <00asgaroth00@gmail.com>
2024-02-26 21:39:24 +0000

    testing

--- home-manager/capabilities/cli/btop/default.nix
+++ home-manager/capabilities/cli/btop/default.nix
@@ -1,19 +1,23 @@
-{ pkgs, ... }: {
-
+{
+  lib,
+  pkgs,
+  ...
+}: {
   programs.btop = {
     enable = true;
     settings = {
-      color_theme = "tokyo-night";
+      color_theme = lib.mkDefault "tokyo-night";
       vim_keys = true;
     };
   };
 
-  xdg.configFile."btop/themes/tokyo-night.theme".source = pkgs.fetchFromGitHub
+  xdg.configFile."btop/themes/tokyo-night.theme".source =
+    pkgs.fetchFromGitHub
     {
       owner = "aristocratos";
       repo = "btop";
       rev = "master";
       sha256 = "sha256-9tSSfgJ8d3Gz/nH6AWlF8+4y3KvD8JnMLOW2WfJ0k6c=";
-    } + "/themes/tokyo-night.theme";
-
+    }
+    + "/themes/tokyo-night.theme";
 }
