383064cf465dda28e3d277ac587836fedbf21817
Asgaroth <00asgaroth00@gmail.com>
2024-02-27 10:35:10 +0000

    testing

--- home-manager/capabilities/desktop/vscodium/default.nix
+++ home-manager/capabilities/desktop/vscodium/default.nix
@@ -1,8 +1,27 @@
-{ pkgs, lib, ... }: {
-
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  custom-extensions = {
+    kamailio-syntax = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+      publisher = "miconda";
+      name = "kamailio-syntax";
+      version = "1.0.8";
+      sha256 = "sha256-8wGof/ESIjWk31U4iIl+oYoGD8B+ToTVucqnGK0RYGM=";
+    };
+  };
+in {
   programs.vscode = {
     enable = true;
+    enableExtensionUpdateCheck = true;
     userSettings = {
+      "[nix]"."editor.tabSize" = 2;
+      "editor.formatOnSave" = true;
+      "files.insertFinalNewline" = true; # Make sure to have a final new line at end of files
+      "files.trimFinalNewlines" = true; # Trim superfluous new lines at end of files
+      "files.trimTrailingWhitespace" = true; # Trim whitespace at end of lines on save
+      "telemetry.telemetryLevel" = "off";
       "window.titleBarStyle" = "custom";
       "workbench.colorTheme" = lib.mkForce "Tokyo Night";
     };
@@ -17,6 +36,7 @@
       ms-vscode-remote.remote-ssh
       timonwong.shellcheck
       yzhang.markdown-all-in-one
+      custom-extensions.kamailio-syntax
     ];
   };
 
@@ -24,5 +44,4 @@
     windowrule = tile, title:VSCodium
     bind = $mainMod, K, exec, codium
   '';
-
 }
