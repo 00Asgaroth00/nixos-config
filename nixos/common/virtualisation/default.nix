041904751e5b1812cdddd130414b5150febad328
Asgaroth <00asgaroth00@gmail.com>
2024-03-11 22:01:15 +0000

    testing

--- nixos/common/virtualisation/default.nix
+++ nixos/common/virtualisation/default.nix
@@ -5,23 +5,47 @@
   username,
   ...
 }: {
-  users.users.${username}.extraGroups = ["docker"];
+  users.users.${username}.extraGroups = ["docker" "libvirtd"];
 
-  virtualisation.docker = {
-    enable = true;
-    enableOnBoot = true;
-    logDriver = "journald";
-    storageDriver = "zfs";
-    # extraPackages = with pkgs; [
-    #   docker-buildx
-    #   docker-compose
-    # ];
-    extraOptions = "--insecure-registry harbor.home.ie";
-    extraPackages = lib.mkMerge [
-      [pkgs.docker-buildx pkgs.docker-compose]
-      (lib.mkIf (config.networking.hostName == "arakasi") [pkgs.nvidia-docker])
-    ];
+  virtualisation = {
+    docker = {
+      enable = true;
+      enableOnBoot = true;
+      logDriver = "journald";
+      storageDriver = "zfs";
+      # extraPackages = with pkgs; [
+      #   docker-buildx
+      #   docker-compose
+      # ];
+      extraOptions = "--insecure-registry harbor.home.ie";
+      extraPackages = lib.mkMerge [
+        [pkgs.docker-buildx pkgs.docker-compose]
+        (lib.mkIf (config.networking.hostName == "arakasi") [pkgs.nvidia-docker])
+      ];
+    };
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [pkgs.OVMFFull.fd];
+      };
+    };
+    spiceUSBRedirection.enable = true;
   };
+
+  services.spice-vdagentd.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    virt-viewer
+    spice
+    spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
+    gnome.adwaita-icon-theme
+  ];
 
   # virtualisation.vmware = {
   #   host = {
